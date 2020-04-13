895af05dcc78d2f9bf68786667b2d9dcfeb8fb14
vijaya bhaskar <vijaya.bhattiprolu@evry.com>
2020-04-13 11:47:54 +0530

    show message

--- EmployeeManagement/Form1.cs
+++ EmployeeManagement/Form1.cs
@@ -22,5 +22,10 @@ namespace EmployeeManagement
             //show form
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi");
+        }
     }
 }
